0c19a11b96aa6e68ba49dd75c8b9282cfb7b0bad
Muhammad Saad <160267671+sheikhmohammedsaadzubari@users.noreply.github.com>
2024-03-06 01:40:12 +0500

    Add files via upload

---
+++ project00_calculator/projects.js
@@ -0,0 +1,94 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const sleepAndStart = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+async function rainbowTitle() {
+    let addRainbowTitle = chalkAnimation.rainbow("Let's start calculation!");
+    await sleepAndStart();
+    addRainbowTitle.stop();
+    let templateCalc = chalk.cyanBright(`
+     _____________________
+    |  _________________  |
+    | | @Saad        0. | |
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    | | x^| √ |Off| | C | |
+    | |___|___|___| |___| |
+    |_____________________|
+    `);
+    console.log(templateCalc);
+}
+await rainbowTitle();
+async function askQuestion() {
+    const answers = await inquirer.prompt([
+        {
+            type: "list",
+            name: "Operator",
+            message: "Which operation do you want to perform?",
+            choices: ["+ Addition", "- Subtraction", "* Multiplication", "/ Division", "^ Power", "√ Square Root"],
+        },
+        {
+            type: "input",
+            name: "num1",
+            message: "Enter the first number:",
+            validate: (input) => {
+                if (input.trim().toLowerCase() === "power") {
+                    return "Please enter a valid number as the base.";
+                }
+                return true;
+            },
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "Enter the exponent (if performing a power operation):",
+            when: (answers) => answers.Operator === "^ Power",
+        },
+    ]);
+    if (answers.Operator === "+ Addition") {
+        console.log(chalk.blueBright(`${answers.num1} + ${answers.num2} = ${answers.num1 + answers.num2}`));
+    }
+    else if (answers.Operator === "- Subtraction") {
+        console.log(chalk.blueBright(`${answers.num1} - ${answers.num2} = ${answers.num1 - answers.num2}`));
+    }
+    else if (answers.Operator === "* Multiplication") {
+        console.log(chalk.blueBright(`${answers.num1} * ${answers.num2} = ${answers.num1 * answers.num2}`));
+    }
+    else if (answers.Operator === "/ Division") {
+        console.log(chalk.blueBright(`${answers.num1} / ${answers.num2} = ${answers.num1 / answers.num2}`));
+    }
+    else if (answers.Operator === "^ Power") {
+        console.log(chalk.blueBright(`${answers.num1} ^ ${answers.num2} = ${Math.pow(Number(answers.num1), answers.num2)}`));
+    }
+    else if (answers.Operator === "√ Square Root") {
+        console.log(chalk.blueBright(`√${answers.num1} = ${Math.sqrt(Number(answers.num1))}`));
+    }
+    else {
+        console.log(chalk.red("Invalid operation selected. Please choose a valid operation."));
+    }
+}
+async function startAgain() {
+    do {
+        await askQuestion();
+        var restart = await inquirer
+            .prompt({
+            type: "input",
+            name: "restart",
+            message: "Do you want to continue? Type 'continue' to continue and 'no' to uncontinue:"
+        });
+    } while (restart.restart.toLowerCase() === "continue");
+}
+startAgain();
